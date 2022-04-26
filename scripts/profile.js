ee755e02a37ad65605dcd892cec03894f38c6da0
Ruslan Afinshakov <shador@mail.ru>
2022-04-26 19:59:16 +1000

    feat: добавлены классы для работы с JS

--- scripts/profile.js
+++ scripts/profile.js
@@ -9,14 +9,3 @@ buttonCourses.addEventListener('click', function () {
 
 //////////// Работа с кнопками форм ////////////
 const profilePage = document.querySelector('.profile-content');
-
-const formButtons = profilePage.querySelectorAll('.form__submit-btn'); //Выбираем все кнопки
-
-function changeButton (elem) {
-  elem.classList.remove('form__submit-btn_type_unsubmitted');
-  elem.classList.add('form__submit-btn_type_submitted');
-}
-
-// formButtons.forEach((element) => {
-//   element.addEventListener('click', changeButton (element));
-// })
